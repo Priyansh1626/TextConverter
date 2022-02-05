23ef3e42c06d0ce68c22c299050dde3486865ade
Priyansh Pandey <priyanshpandey2055@gmail.com>
2022-02-06 00:46:57 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      // showAlert("Mode changed", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      // showAlert("Mode changed", "success");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-2">
+        <Form heading='Enter Text To Analyze' mode={mode} showAlert={showAlert} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Form(props) {
+
+    const [text, setText] = useState("");
+
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLoClick = () => {
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+
+    const clearTextArea = () => {
+        setText("");
+    }
+
+    const handleCopy = () => {
+        let text = document.getElementById("textArea");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to clip board", "success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    return (
+        <>
+            <div className="container" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h1 className="my-2"> {props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'light' ? 'white' : '#0b1f2d', color: props.mode === 'light' ? 'black' : 'white' }} id="textArea" rows="8" >{text}</textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleUpClick}>
+                    Convert To Uppercase
+                </button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+                    Convert To LowerCase
+                </button>
+                <button className="btn btn-danger" onClick={clearTextArea}>
+                    Clear
+                </button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>
+                    Copy to clipboard
+                </button>
+                <button className="btn btn-primary" onClick={handleExtraSpaces}>
+                    Remove extra spaces
+                </button>
+            </div>
+            <div className="container my-3" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+                <h2 className='my-3'>
+                    Text Summary
+                </h2>
+                <p className='my-2'>
+                    {text.split(" ").length} words(including space) and {text.length} characters.
+                </p>
+                <p className='my-2'>
+                    It will take {0.008 * text.split(" ").length} words per minute to read
+                </p>
+                <div style={{ minHeight: '20vh' }}>
+                    <h2>
+                        Preview
+                    </h2>
+                    <p className='my-2'>
+                        {text.length > 0 ? text : 'Enter some text to preview it here'}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+Form.propTypes = {
+    heading: PropTypes.string
+}
+
+Form.defaultProps =
+{
+    heading: " Add heading here"
+}
